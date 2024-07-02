0c6797d5c285bf30d4a942056aae8081686fcaac
dvtung <122477565+dinh-van-tung@users.noreply.github.com>
2024-07-02 19:00:34 +0700

    Create Boring_Day.cpp

---
+++ Boring_Day.cpp
@@ -0,0 +1,51 @@
+// DINH VAN TUNG
+// https://codeforces.com/problemset/problem/1982/C
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <time.h>
+#include <vector>
+#include <string>
+#include <map>
+#include <string>
+#include <stack>
+#include <iomanip>
+#include <bitset>
+#include <random>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+#define MOD 20020303
+#define N 200009
+typedef long long ll;
+typedef long double ld;
+typedef double db;
+// typedef unsigned long long ll;
+
+int t, n;
+ll l, r, a[N];
+
+int main()
+{
+     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+     //freopen("STRING.inp", "r", stdin); freopen("STRING.out", "w", stdout);
+     //freopen("VTung.input", "r", stdin); freopen("VTung.output", "w", stdout);
+
+     cin >> t;
+     while (t--)
+     {
+          cin >> n >> l >> r;
+          for (int i = 1; i <= n; i++) cin >> a[i];
+
+          ll s = 0, ans = 0, left = 1;
+          for (int i = 1; i <= n; i++)
+          {
+               s += a[i];
+               while (s > r && left <= i) s -= a[left++];
+               if (l <= s && s <= r && left <= i) ans++, s = 0, left = i + 1;
+          }
+          cout << ans << '\n';
+     }
+     return 0;
+}
+// DINH VAN TUNG
